8ffb30986fc08f5329f6491a6dc55f79e03f444f
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-11-03 21:13:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const submitData = (name, email) => {
+    const userInfo = {
+        name: `${name}`,
+        email: `${email}`
+    }
+   return (
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then((resp) => resp.json())
+    .then((data) => document.body.append(data.id))
+    .catch((error) => document.body.append(error.message))
+   )
+
+}
